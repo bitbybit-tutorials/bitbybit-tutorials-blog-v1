b8d337c416bca16f1f4b5f24b05a7d0f40539dfd
Bitbybit Tutorials <bitbybit_tutorials@yahoo.com>
2022-02-05 21:50:55 +0000

    Fix type error

--- apps/web/src/modules/posts/posts-list.tsx
+++ apps/web/src/modules/posts/posts-list.tsx
@@ -3,7 +3,7 @@ import PostItem from "./posts-list-item";
 type Props = {
   posts: Post[];
   offset?: number;
-  type?: "featured" | "full-width" | "regular";
+  type?: "full-width" | "regular";
 };
 
 export default function PostsList({ posts, offset, type }: Props) {
